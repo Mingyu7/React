0fd729bb13b47fdb512b4c05174b4c99fb1544e5
mingyu <vkans7526@naver.com>
2025-08-18 11:26:35 +0900

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Hello from './Hello';
+import Profile from "./Profile";
+import Welcome from './Wellcome';
 
 // App.jsx
 function App() {
   const name = "민규";
   return (
     <div>
-      <h1>안녕하세요! 저는 {name}입니다.</h1>
-      <p>전공: 컴퓨터공학</p>
-      <p>취미: 게임 개발,야구보기</p> 
-
+      <h4>Welcome.jsx</h4>
+      <Welcome name="민규" />
+    
+      
+      <h4> Hello.jsx</h4>
       <Hello name={name} />
       <p>React와 Vite를 사용하여 개발 중입니다1.</p>
       <Hello name="민수" />
       <p>React와 Vite를 사용하여 개발 중입니다2.</p>
+
+      <h4>Profile.jsx</h4>
+      <Profile name="민수" major="디자인" hobby="독서"/>
+      <Profile name="민규" major="컴퓨터공학" hobby="게임" />
+      
     </div>
   );
 }
